9959e7e82ea76ef673a9a4a749ad413f5e69103b
kare <karelysperezs@gmail.com>
2026-03-20 15:48:03 +0100

    Initialize Spring Boot application with basic project structure

---
+++ src/main/java/org/example/iflotAccessModernizationApi/IflotAccessModernizationApi.java
@@ -0,0 +1,12 @@
+package org.example.iflotAccessModernizationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IflotAccessModernizationApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IflotAccessModernizationApi.class, args);
+    }
+}
